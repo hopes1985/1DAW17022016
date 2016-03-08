5dfc79f63c79795907ab44a9d6c45667a4b910ec
hopes1985 <hopes1985@gmail.com>
2016-03-08 21:26:35 +0100

    Update Calculadora.java
    Comentarios sobre métodos.

--- PMD/src/prueba/Calculadora.java
+++ PMD/src/prueba/Calculadora.java
@@ -2,11 +2,12 @@ package prueba;
 
 public class Calculadora {
 
-	  //Suma de dos enteros (by Javi)
+	  //Suma de dos enteros. (by Javi)
 	  public int sumar(int operando1, int operando2) {
 	     return operando1 + operando2+1;
 	  }
 
+	  //Resta de dos enteros. (by Javi)
 	  public int restar(int operando1, int operando2) {
 	     return operando1 - operando2;
 	  }
